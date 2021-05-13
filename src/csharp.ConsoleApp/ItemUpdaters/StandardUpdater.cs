214a5355ce617c8e31026198b2ef845e7c6cc8cb
alwaysintune <domantas.slenys@gmail.com>
2021-05-13 14:31:09 +0300

    adding Standard Items updater

---
+++ src/csharp.ConsoleApp/ItemUpdaters/StandardUpdater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace csharp.ConsoleApp.ItemUpdaters
+{
+    public class StandardUpdater : ItemUpdater
+    {
+        public StandardUpdater()
+            : base(qualityChange: -1)
+        {
+            RegexPattern = nameof(StandardUpdater);
+        }
+
+        public override void UpdateQuality(Item item)
+        {
+            base.UpdateQuality(item);
+
+            item.Quality = Math.Max(0,
+                item.Quality + QualityChange * (item.SellIn < 0 ? 2 : 1));
+        }
+    }
+}
