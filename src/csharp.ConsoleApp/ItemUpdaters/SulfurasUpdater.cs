dc37d15fb9f1a55c83562b5f64e54f59b1a2603e
alwaysintune <domantas.slenys@gmail.com>
2021-05-13 14:24:04 +0300

    adding Sulfuras updater

---
+++ src/csharp.ConsoleApp/ItemUpdaters/SulfurasUpdater.cs
@@ -0,0 +1,16 @@
+﻿namespace csharp.ConsoleApp.ItemUpdaters
+{
+    public class SulfurasUpdater : ItemUpdater
+    {
+        public SulfurasUpdater() : base(qualityChange: 0)
+        {
+            RegexPattern = "Sulfuras.*";
+        }
+
+        public override void UpdateQuality(Item item)
+        {
+            if (item.Quality != 80)
+                item.Quality = 80;
+        }
+    }
+}
