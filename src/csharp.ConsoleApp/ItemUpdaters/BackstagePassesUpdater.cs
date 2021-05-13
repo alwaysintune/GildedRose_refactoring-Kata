9b73d0852d5d0f73f3db811428af89d1afea8618
alwaysintune <domantas.slenys@gmail.com>
2021-05-13 14:29:36 +0300

    adding Backstage Passes updater

---
+++ src/csharp.ConsoleApp/ItemUpdaters/BackstagePassesUpdater.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp.ConsoleApp.ItemUpdaters
+{
+    public class BackstagePassesUpdater : ItemUpdater
+    {
+        public BackstagePassesUpdater()
+            : base(qualityChange: 0)
+        {
+            RegexPattern = @"Backstage passes.*";
+        }
+
+        public override void UpdateQuality(Item item)
+        {
+            base.UpdateQuality(item);
+
+            if (item.SellIn >= 10)
+                item.Quality += 1;
+            else if (item.SellIn >= 5)
+                item.Quality += 2;
+            else if (item.SellIn >= 0)
+                item.Quality += 3;
+            else
+                item.Quality = 0;
+
+            item.Quality = Math.Min(50, item.Quality);
+        }
+    }
+}
