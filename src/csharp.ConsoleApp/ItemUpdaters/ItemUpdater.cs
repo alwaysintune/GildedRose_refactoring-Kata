6a0f6b87fc60189d94c3814938248103515f43a8
alwaysintune <domantas.slenys@gmail.com>
2021-05-13 14:05:41 +0300

    adding abstraction for quality updating

---
+++ src/csharp.ConsoleApp/ItemUpdaters/ItemUpdater.cs
@@ -0,0 +1,19 @@
+﻿namespace csharp.ConsoleApp.ItemUpdaters
+{
+    public abstract class ItemUpdater
+    {
+        protected int QualityChange { get; private set; }
+
+        public string RegexPattern { get; protected set; }
+
+        public ItemUpdater(int qualityChange)
+        {
+            QualityChange = qualityChange;
+        }
+
+        public virtual void UpdateQuality(Item item)
+        {
+            item.SellIn -= 1;
+        }
+    }
+}
