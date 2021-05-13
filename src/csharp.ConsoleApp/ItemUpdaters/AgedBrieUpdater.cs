48fc2c6ff2e79372816b98c9b80addab0067c2e0
alwaysintune <domantas.slenys@gmail.com>
2021-05-13 14:26:57 +0300

    adding Aged Brie updater

---
+++ src/csharp.ConsoleApp/ItemUpdaters/AgedBrieUpdater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace csharp.ConsoleApp.ItemUpdaters
+{
+    public class AgedBrieUpdater : ItemUpdater
+    {
+        public AgedBrieUpdater()
+            : base(qualityChange: 1)
+        {
+            RegexPattern = @"Aged Brie";
+        }
+
+        public override void UpdateQuality(Item item)
+        {
+            base.UpdateQuality(item);
+
+            item.Quality = Math.Min(50,
+                item.Quality + QualityChange * (item.SellIn < 0 ? 2 : 1));
+        }
+    }
+}
